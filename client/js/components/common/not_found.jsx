5fe391ba2289978af5e05c80e4f9a062069b7fa5
Justin Ball <justinball@gmail.com>
2017-03-23 20:57:45 -0600

    Removes old files

--- client/js/components/common/not_found.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <div>
-    <h1>Page Not Found</h1>
-    <p>Sorry, but the page you were trying to view does not exist.</p>
-  </div>
-);
--- client/js/reducers/application.js
+++
@@ -1,9 +0,0 @@
-const initialState = {};
-
-export default (state = initialState, action) => {
-  switch (action.type) {
-
-    default:
-      return state;
-  }
-};
