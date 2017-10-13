9585557c32a0a848cf8887341d2d7aaad0edc78f
dittonjs <jditton.atomic@gmail.com>
2017-10-13 14:39:02 -0600

    remove blank line

--- client/apps/hello_world/components/common/not_found.spec.jsx
+++ client/apps/hello_world/components/common/not_found.spec.jsx
@@ -1,7 +1,6 @@
 import React     from 'react';
 import TestUtils from 'react-dom/test-utils';
 import Stub      from 'atomic-fuel/libs/specs_support/stub';
-
 import NotFound  from './not_found';
 
 describe('not found', () => {
